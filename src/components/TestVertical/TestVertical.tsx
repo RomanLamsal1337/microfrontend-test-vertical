bb2961492c10b8acccfef7cd7ec637a0dd566123
Robin Müller <robin.mueller@aclue.de>
2021-11-02 17:57:57 +0100

    component type

--- src/components/TestVertical/TestVertical.tsx
+++ src/components/TestVertical/TestVertical.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './TestVertical.css';
 
-const TestVertical = () => {
+const TestVertical: React.FC = () => {
   return (
     <div className="App">
       Hello Workshop
